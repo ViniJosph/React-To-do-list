01b42f87f3ad1894d9f67d5065192bd1b2cccf58
Josph <viniciusjose38@hotmail.com>
2022-05-01 21:40:53 -0300

    Agora vai fml

---
+++ build/precache-manifest.4dc21cd5e2713fb36d50279c09cc3a25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0da6284b5e6fc120975d32cd988420d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ebbaee9509d1322a98",
+    "url": "/static/css/main.7d218cf8.chunk.css"
+  },
+  {
+    "revision": "cbd9dc68eceaccea24e5",
+    "url": "/static/js/2.9880ada7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9880ada7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b9ebbaee9509d1322a98",
+    "url": "/static/js/main.d0a797d9.chunk.js"
+  },
+  {
+    "revision": "9fdbd592b5644bc57f7c",
+    "url": "/static/js/runtime-main.f084f130.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
